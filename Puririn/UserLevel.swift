d4521ac9a8de4b3d475d11e84f730714e41f50c9
Victor <victoraugustolls@gmail.com>
2015-05-03 06:12:33 -0300

    Ta em falta

--- Puririn/UserLevel.swift
+++ Puririn/UserLevel.swift
@@ -11,10 +11,36 @@ import Foundation
 class UserLevel {
     
     class func getUserLevel() -> Int {
-        return 0
+        
+        var level = 0
+        
+        var pathAux = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        var path: NSString = pathAux.stringByAppendingPathComponent("qualquerCoisa.plist")
+        var fileManager = NSFileManager.defaultManager()
+        if (!(fileManager.fileExistsAtPath(path as String)))
+        {
+            var bundle: NSString = NSBundle.mainBundle().pathForResource("qualquerCoisa", ofType: "pList")!
+            fileManager.copyItemAtPath(bundle as String, toPath: path as String, error: nil)
+        }
+        let contents:NSDictionary! = NSDictionary(contentsOfFile: path as String)
+        level = contents.objectForKey("Level") as! Int
+        
+        return level
     }
    
     class func setUserLevel(level: Int) {
+        
+        var pathAux = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        var path = pathAux.stringByAppendingPathComponent("qualquerCoisa.plist")
+        var fileManager = NSFileManager.defaultManager()
+        if (!(fileManager.fileExistsAtPath(path)))
+        {
+            var bundle: NSString = NSBundle.mainBundle().pathForResource("qualquerCoisa", ofType: "pList")!
+            fileManager.copyItemAtPath(bundle as String, toPath: path, error: nil)
+        }
+        var data : NSMutableDictionary! = NSMutableDictionary(contentsOfFile: path)
+        data.setObject("0", forKey: "Level")
+        data.writeToFile(path, atomically: false)
         
     }
     
